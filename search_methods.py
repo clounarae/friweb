b91791719a7b0ba0854a27428c0077fa1718a639
JihaneBennis <bennis.jihane@hotmail.fr>
2019-03-16 16:59:26 +0100

    time elapsed per query in boolean and vectorial model

--- search_methods.py
+++ search_methods.py
@@ -7,6 +7,7 @@ from utils import split_documents,\
                   intersection
 from text_processing import tokenize,\
                             get_all_doc_id
+import time
 
 
 def print_query_result(result):
@@ -16,6 +17,7 @@ def print_query_result(result):
 
 
 def input_query_vectorial_model(docs_coordinates, vocabulary, inverted_index, path, weight_function, n_results=10):
+    start_time = time.clock()
     doc_dict = split_documents(path)
     n_documents = get_number_of_documents(path)
     request_id = 0
@@ -23,10 +25,14 @@ def input_query_vectorial_model(docs_coordinates, vocabulary, inverted_index, pa
     docs_coordinates[request_id] = np.asarray([weight_function(request_id, term, doc_dict, inverted_index, n_documents) for term in vocabulary])
     similarity_dict = {docID : compute_cos_similarity(request_id, docID, docs_coordinates) for docID in doc_dict.keys()}
     similarity_to_query = sorted(similarity_dict.items(), key=operator.itemgetter(1), reverse=True)
-    return [result[0] for result in similarity_to_query[1:n_results+1] if result[1] > 0]
+    results = [result[0] for result in similarity_to_query[1:n_results+1] if result[1] > 0]
+    elapsed_time = time.clock() - start_time
+    print('vectorial search took {} s to run the query'.format(elapsed_time))
+    return results
 
 
 def boolean_search(client_request, inverted_index, collection_path, n_results=10):
+    time_start = time.clock()
     all_doc_id = get_all_doc_id(collection_path)
     request = tokenize(client_request)
     final_docs = []
@@ -102,4 +108,6 @@ def boolean_search(client_request, inverted_index, collection_path, n_results=10
         for j in range(len(index_treated)):
             request.pop(index_treated[1])
         final_docs = documents
+        time_elapsed =  time.clock()-time_start
+        print('boolean search took {} s to run the query'.format(time_elapsed))
     return final_docs[:n_results]
